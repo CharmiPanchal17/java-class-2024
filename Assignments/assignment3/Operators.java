87436208bc5d5b17bf16e209db55294d8f0568e6
CharmiPanchal17 <charmipanchal17@gmail.com>
2024-03-23 18:37:30 +0300

    assignment3

---
+++ Assignments/assignment3/Operators.java
@@ -0,0 +1,18 @@
+// Operators in java include;
+
+// Arithmetical operators- Addition (+)
+//                         Subtraction (-)
+//                         Multiplication (*)
+//                         Division (/)
+//                         Modulus (%): Returns the remainder after division.
+//                         Increment (++)
+
+// Comparision operators-Equal to (==): Checks if two values are equal.
+//                       Not equal (!=): Checks if two values are not equal.
+//                       Greater than (>), Less than (<), Greater than or equal to (>=), and Less than or equal to (<=).
+
+// Assignment operators-Simple Assignment (=): Assigns a value to a variable.
+//                      Addition Assignment (+=): Adds a value to a variable.
+                      
+// Logical operators-Logical AND (&&): Returns true if both statements are true.
+//                   Logical OR (||): Returns true if at least one statement is true.
