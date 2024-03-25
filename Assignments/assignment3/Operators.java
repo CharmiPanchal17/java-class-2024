d5999f9a5772afe9457a17fd0856884e0c104d0e
CharmiPanchal17 <charmipanchal17@gmail.com>
2024-03-25 22:10:53 +0300

    assignment4

--- Assignments/assignment3/Operators.java
+++ Assignments/assignment3/Operators.java
@@ -1,22 +1,22 @@
-// Operators in java include;
+/* Operators in java include;
 
-// Arithmetical operators- Addition (+)
-//                         Subtraction (-)
-//                         Multiplication (*)
-//                         Division (/)
-//                         Modulus (%): Returns the remainder after division.
-//                         Increment (++)
+Arithmetical operators- Addition (+)
+                        Subtraction (-)
+                        Multiplication (*)
+                        Division (/)
+                        Modulus (%): Returns the remainder after division.
+                        Increment (++)
 
-// Comparision operators-Equal to (==): Checks if two values are equal.
-//                       Not equal (!=): Checks if two values are not equal.
-//                       Greater than (>), Less than (<), Greater than or equal to (>=), and Less than or equal to (<=).
+Comparision operators-Equal to (==): Checks if two values are equal.
+                      Not equal (!=): Checks if two values are not equal.
+                      Greater than (>), Less than (<), Greater than or equal to (>=), and Less than or equal to (<=).
 
-// Assignment operators-Simple Assignment (=): Assigns a value to a variable.
-//                      Addition Assignment (+=): Adds a value to a variable.
+Assignment operators-Simple Assignment (=): Assigns a value to a variable.
+                     Addition Assignment (+=): Adds a value to a variable.
                       
-// Logical operators-Logical AND (&&): Returns true if both statements are true.
-//                   Logical OR (||): Returns true if at least one statement is true.
-
+Logical operators-Logical AND (&&): Returns true if both statements are true.
+                  Logical OR (||): Returns true if at least one statement is true.
+*/ 
 
 // Examples of arithmetical operators
 
