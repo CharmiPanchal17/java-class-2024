4dd7285c240f2dd1b0e8f5df31196b4b389a266a
CharmiPanchal17 <charmipanchal17@gmail.com>
2024-03-29 20:14:49 +0300

    assignment7 forloop

---
+++ Loops/ForLoop.java
@@ -0,0 +1,27 @@
+
+public class ForLoop{
+    public static void main(String[] args) {
+
+        // for loop example 
+        for( int x = 1; x<= 10; x++){
+            System.out.println(x);
+        }
+
+        System.out.print("\n");
+
+        // this is decrementing.. 
+        for(int i =10; i>= 1; i--){
+            System.out.println(i);
+        }
+
+        System.out.print("\n");
+        
+        // for even numbers
+        for(int y=1; y<= 10; y++){
+            if(y%2==0){
+                System.out.println(y);
+            }
+        }
+    }
+}
+
